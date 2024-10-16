1302bb6e1e5e7bed45c9ffc05e5419d9bb854026
barash-romash <165959539+barash-romash@users.noreply.github.com>
2024-10-16 21:42:46 +0300

    Небольшие исправления
    - Метод get_cur_dir_str теперь возвращает путь без '/' в начале;
    - Вместо пользователей User1 и User2 теперь root и User1;
    - Доработан метод ls

--- ShellEmulator.py
+++ ShellEmulator.py
@@ -6,7 +6,7 @@ from pprint import pprint
 class ShellEmulator:
     def __init__(self, config_file):
         self.file_system_path = self.get_file_path(config_file)
-        self.users = ["User1", "User2"]
+        self.users = ["root", "User1"]
         self.users_files = {self.users[0]: [],
                             self.users[1]: []}
         self.user_name = self.users[0]
@@ -24,10 +24,22 @@ class ShellEmulator:
         root = tree.getroot()
         return root.find('file_system_path').text
 
-    def ls(self):
-        for i in self.get_dir_file_list():
-            print(*i, end='  ')
-        print()
+    def ls(self, arg=''):
+        files = {}
+        if arg == '-l':
+            for [file] in self.get_dir_file_list():
+                path_of_file = self.get_cur_dir_str() + file
+                for owner in self.users:
+                    if path_of_file in self.users_files[owner]:
+                        files[file] = owner
+            for file, owner in files.items():
+                print(owner, file)
+        elif arg != '':
+            print("Неизвестная опция", arg)
+        else:
+            for [file] in self.get_dir_file_list():
+                print(file, end='  ')
+            print()
 
     def get_dir_file_list(self):
         fl = [f for f in self.sfl if len(f) == len(self.cur_dir) + 1]
@@ -57,15 +69,13 @@ class ShellEmulator:
             with tarfile.open(self.file_system_path, 'r') as tar:
                 if [path_split[0]] in self.get_dir_file_list() and \
                         (self.cur_dir + path_split) in self.get_sys_file_list() \
-                        and tar.getmember(
-                    self.get_cur_dir_str()[1:] + path).isdir():  # Не работает с относительными адресами
+                        and tar.getmember(self.get_cur_dir_str() + path).isdir():
                     self.cur_dir.extend(path_split)
                 else:
                     print("Не удаётся найти путь")
-            # TODO(Решить проблему перемещения в директорию в виде файла)
 
     def get_cur_dir_str(self):
-        cur_dir_str = '/'
+        cur_dir_str = ''
         for i in self.cur_dir:
             cur_dir_str += i + '/'
         return cur_dir_str
@@ -79,7 +89,7 @@ class ShellEmulator:
     def chown(self, arg):
         owner, filename = arg.split()
         if owner in self.users_files:
-            path = self.get_cur_dir_str()[1:] + filename
+            path = self.get_cur_dir_str() + filename
             if path in tarfile.open(self.file_system_path).getnames():
                 if path not in self.users_files[owner]:
                     for user in self.users:
@@ -97,7 +107,7 @@ class ShellEmulator:
         if len(path) == 0:
             return
         if path[0] != '/':
-            path = self.get_cur_dir_str()[1:] + path
+            path = self.get_cur_dir_str() + path
         with tarfile.open(self.file_system_path, 'r') as tar:
             try:
                 file = tar.getmember(path)
@@ -107,20 +117,25 @@ class ShellEmulator:
             if not file.isfile():
                 print("Файл не найден")
                 return
-            l = set()
+            lines = set()
             for line in tar.extractfile(path):
-                l.add(line.decode().strip())
-            for i in l:
+                lines.add(line.decode().strip())
+            for i in lines:
                 print(i)
 
     def run(self):
         while True:
-            line_start = f"{self.user_name}{self.get_cur_dir_str()}:~ $"
+            line_start = f"{self.user_name}/{self.get_cur_dir_str()}:~ $"
             command = input(line_start).split(' ', 1)
-            if command[0] == 'exit':
+            if len(command) == 0:
+                continue
+            elif command[0] == 'exit':
                 break
             elif command[0] == 'ls':
-                self.ls()
+                if len(command) == 2:
+                    self.ls(command[1])
+                else:
+                    self.ls()
             elif command[0] == 'pwd':
                 self.pwd()
             elif command[0] == 'cd':
@@ -135,7 +150,7 @@ class ShellEmulator:
             elif command[0] == "sufl":
                 pprint(self.users_files)
             else:
-                print("Команда не найдена: ", command[0])
+                print("Команда не найдена:", command[0])
 
 
 emulator = ShellEmulator('config.xml')
